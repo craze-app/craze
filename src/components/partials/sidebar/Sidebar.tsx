d590db67803c6457c36ed4c404b553d73a4c2531
Orçun Tuna <turboindirblog@gmail.com>
2022-12-31 11:07:22 +0300

    add search to sidebar

--- src/components/partials/sidebar/Sidebar.tsx
+++ src/components/partials/sidebar/Sidebar.tsx
@@ -1,16 +1,40 @@
-import React from "react";
+import React, {useMemo, useState} from "react";
 import styles from "./Sidebar.module.scss"
 import cn from "classnames"
 import {Link} from "react-router-dom";
-import {features} from "../../../features";
+import {Feature, features} from "../../../features";
+import Fuse from "fuse.js";
 
-const Sidebar = ({backgroundColor}: { backgroundColor: string }) => {
+const fuseOptions = {
+  keys: [
+    "title",
+    "id"
+  ]
+}
+
+type SidebarProps = {
+  backgroundColor: string
+}
+
+const Sidebar = (props: SidebarProps) => {
+
+  const [searchQuery, setSearchQuery] = useState<string>("")
+  const fuse = new Fuse(features, fuseOptions);
+
+  const results: Feature[] = useMemo(() => {
+    if(searchQuery === ""){
+      return features
+    }
+    return fuse.search(searchQuery).map((item) => item.item)
+  }, [searchQuery])
 
   return (
-    <div className={styles.sidebar} style={{background: backgroundColor}}>
+    <div className={styles.sidebar} style={{background: props.backgroundColor}}>
       <div className={cn(styles.sidebarHeader, "draggable-area")} />
       <div className={styles.sidebarSearch}>
         <input
+          value={searchQuery}
+          onChange={e => setSearchQuery(e.target.value)}
           className={styles.sidebarSearchInput}
           placeholder="Search..."
         />
@@ -19,7 +43,7 @@ const Sidebar = ({backgroundColor}: { backgroundColor: string }) => {
         </div>
       </div>
       <div className={cn(styles.sidebarInside, styles.scrollbar)}>
-        {features.map(Feature => (
+        {results.map(Feature => (
           <Link key={Feature.id} to={`/features/${Feature.id}`} className={styles.menuItem}>
             <Feature.sidebarIcon className={""} size={16}/>
             {Feature.title}
