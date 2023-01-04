9f79601a5b7d4d0f0193e2f5896523055e0b103a
Orçun Tuna <turboindirblog@gmail.com>
2023-01-04 23:29:58 +0300

    add eslint and prettier config

--- src/features/url-encode-decode/UrlEncodeDecode.tsx
+++ src/features/url-encode-decode/UrlEncodeDecode.tsx
@@ -1,25 +1,23 @@
-import styles from "./UrlEncodeDecode.module.scss"
-import InputBar from "../../components/organisms/input-bar/InputBar";
-import {
-  ReflexContainer,
-  ReflexSplitter,
-  ReflexElement
-} from 'react-reflex'
-import {useMemo, useState} from "react";
-import AceEditor from "react-ace";
-import OutputBar from "../../components/organisms/output-bar/OutputBar";
-import {UrlEncodeDecodeService} from "./UrlEncodeDecode.service";
-import {UrlEncodeDecodeActions} from "./UrlEncodeDecode.types";
-import {urlEncodeDecodeSample} from "./UrlEncodeDecode.sample";
+import { useMemo, useState } from 'react'
+import AceEditor from 'react-ace'
+import { ReflexContainer, ReflexElement, ReflexSplitter } from 'react-reflex'
 
-import "ace-builds/src-noconflict/mode-text";
-import "ace-builds/src-noconflict/theme-one_dark";
-import "ace-builds/src-noconflict/ext-language_tools";
+import InputBar from '../../components/organisms/input-bar/InputBar'
+import OutputBar from '../../components/organisms/output-bar/OutputBar'
+import styles from './UrlEncodeDecode.module.scss'
+import { urlEncodeDecodeSample } from './UrlEncodeDecode.sample'
+import { UrlEncodeDecodeService } from './UrlEncodeDecode.service'
+import { UrlEncodeDecodeActions } from './UrlEncodeDecode.types'
+
+import 'ace-builds/src-noconflict/ext-language_tools'
+import 'ace-builds/src-noconflict/mode-text'
+import 'ace-builds/src-noconflict/theme-one_dark'
 
 const UrlEncodeDecode = () => {
-
-  const [inputText, setInputText] = useState<string>("")
-  const [actionType, setActionType] = useState<UrlEncodeDecodeActions>(UrlEncodeDecodeActions.ENCODE)
+  const [inputText, setInputText] = useState<string>('')
+  const [actionType, setActionType] = useState<UrlEncodeDecodeActions>(
+    UrlEncodeDecodeActions.ENCODE
+  )
 
   const outputText = useMemo(() => {
     try {
@@ -29,22 +27,32 @@ const UrlEncodeDecode = () => {
         return UrlEncodeDecodeService.decode(inputText)
       }
     } catch (err) {
-      return "Invalid Input"
+      return 'Invalid Input'
     }
-  }, [inputText, actionType]);
+  }, [inputText, actionType])
 
   const RenderInputTypes = () => {
     return (
       <div className={styles.actionTypes}>
         <label htmlFor="action-encode">
-          <input id={"action-encode"} type={"radio"} value={UrlEncodeDecodeActions.ENCODE}
-                 checked={actionType === UrlEncodeDecodeActions.ENCODE}
-                 onChange={(e) => setActionType(e.target.value as UrlEncodeDecodeActions)}/> <span>Encode</span>
+          <input
+            id={'action-encode'}
+            type={'radio'}
+            value={UrlEncodeDecodeActions.ENCODE}
+            checked={actionType === UrlEncodeDecodeActions.ENCODE}
+            onChange={(e) => setActionType(e.target.value as UrlEncodeDecodeActions)}
+          />{' '}
+          <span>Encode</span>
         </label>
         <label htmlFor="action-decode">
-          <input id={"action-decode"} type={"radio"} value={UrlEncodeDecodeActions.DECODE}
-                 checked={actionType === UrlEncodeDecodeActions.DECODE}
-                 onChange={(e) => setActionType(e.target.value as UrlEncodeDecodeActions)}/> <span>Decode</span>
+          <input
+            id={'action-decode'}
+            type={'radio'}
+            value={UrlEncodeDecodeActions.DECODE}
+            checked={actionType === UrlEncodeDecodeActions.DECODE}
+            onChange={(e) => setActionType(e.target.value as UrlEncodeDecodeActions)}
+          />{' '}
+          <span>Decode</span>
         </label>
       </div>
     )
@@ -55,39 +63,45 @@ const UrlEncodeDecode = () => {
       <div className={styles.splitContainer}>
         <ReflexContainer orientation="horizontal">
           <ReflexElement className="pane" minSize={100}>
-            <InputBar onClickPaste={(text) => setInputText(text)} onClickClear={() => setInputText("")}
-                      onClickSample={() => setInputText(urlEncodeDecodeSample[actionType])} rightComponent={<RenderInputTypes/>}/>
+            <InputBar
+              onClickPaste={(text) => setInputText(text)}
+              onClickClear={() => setInputText('')}
+              onClickSample={() => setInputText(urlEncodeDecodeSample[actionType])}
+              rightComponent={<RenderInputTypes />}
+            />
             <AceEditor
               fontSize={13}
-              style={{flex: 1, width: "100%"}}
+              style={{ flex: 1, width: '100%' }}
               mode="text"
               theme="one_dark"
               value={inputText}
-              onChange={text => setInputText(text)}
+              onChange={(text) => setInputText(text)}
               name="url-encode-decode-input"
-              editorProps={{$blockScrolling: true}}
+              editorProps={{ $blockScrolling: true }}
               wrapEnabled={true}
               showGutter={false}
               showPrintMargin={false}
-              placeholder={"Type URL..."}
+              placeholder={'Type URL...'}
             />
           </ReflexElement>
-          <ReflexSplitter/>
+          <ReflexSplitter />
           <ReflexElement className="pane" minSize={100}>
-            <OutputBar copyValue={outputText}/>
+            <OutputBar copyValue={outputText} />
             <AceEditor
               readOnly={true}
               fontSize={13}
-              style={{flex: 1, width: "100%"}}
+              style={{ flex: 1, width: '100%' }}
               mode="text"
               theme="one_dark"
               value={outputText}
               name="url-encode-decode-output"
-              editorProps={{$blockScrolling: true}}
+              editorProps={{ $blockScrolling: true }}
               wrapEnabled={true}
               showGutter={false}
               showPrintMargin={false}
-              placeholder={actionType === UrlEncodeDecodeActions.ENCODE ? "Encoded URL" : "Decoded URL"}
+              placeholder={
+                actionType === UrlEncodeDecodeActions.ENCODE ? 'Encoded URL' : 'Decoded URL'
+              }
             />
           </ReflexElement>
         </ReflexContainer>
